29f6dc34da9c0bf702c19eaa384dbd6c6d23b86b
Nick Isaacs <nickisaacs@gmail.com>
2018-07-21 23:49:14 +0530

    Initial React Setup

---
+++ README.md
@@ -0,0 +1,12 @@
+﻿# Discussion forum in React!
+
+I'm building a discussion forum with React as the front end. It's still WIP (hello world to be exact).
+Hoping to spend some time on this. 
+I'm considering GraphQL, MongoDB, Next.js and Docker
+
+## Run
+
+    cd client
+    yarn install
+    yarn run start (for dev)
+
---
+++ client/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/public/index.html",
+  filename: "./index.html"
+});
+
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundled.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [htmlPlugin]
+};
